22629b99ba451e9099bbe75a91f3f437809e1810
unknown <vivekkhodade01@gmail.com>
2022-11-09 23:10:46 +0530

    Updates

---
+++ precache-manifest.732dbb1afad03f7df81c212b1dd4d9ab.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2d3e9005971b9f13d7b",
+    "url": "/Rohan_Invitation/static/css/main.112072ce.chunk.css"
+  },
+  {
+    "revision": "f2d3e9005971b9f13d7b",
+    "url": "/Rohan_Invitation/static/js/main.f2d3e900.chunk.js"
+  },
+  {
+    "revision": "6da27c3cc7840968dc5b",
+    "url": "/Rohan_Invitation/static/js/runtime~main.6da27c3c.js"
+  },
+  {
+    "revision": "deaf1ea322bc241ba80d",
+    "url": "/Rohan_Invitation/static/css/2.d1016624.chunk.css"
+  },
+  {
+    "revision": "deaf1ea322bc241ba80d",
+    "url": "/Rohan_Invitation/static/js/2.deaf1ea3.chunk.js"
+  },
+  {
+    "revision": "ec959cf2e3c84958206aaf72608ebdb4",
+    "url": "/Rohan_Invitation/static/media/background2.ec959cf2.jpg"
+  },
+  {
+    "revision": "0e211de80c17803da4dd94970c85e733",
+    "url": "/Rohan_Invitation/static/media/first.0e211de8.jpeg"
+  },
+  {
+    "revision": "9e050e136093bf73676534ea3f3a8b15",
+    "url": "/Rohan_Invitation/static/media/ganapati.9e050e13.jpeg"
+  },
+  {
+    "revision": "cdee0b035e5f33636e9dc12ac0c427e6",
+    "url": "/Rohan_Invitation/static/media/logo.cdee0b03.jpg"
+  },
+  {
+    "revision": "441e095c7df435dfa8e8bf0ba2b5871b",
+    "url": "/Rohan_Invitation/static/media/third.441e095c.jpeg"
+  },
+  {
+    "revision": "95999124a7f0634e0793f1539520cc24",
+    "url": "/Rohan_Invitation/static/media/hotel.95999124.jpeg"
+  },
+  {
+    "revision": "18af806bc1de3249ab370f8b81eb9d02",
+    "url": "/Rohan_Invitation/static/media/SANGEET.18af806b.gif"
+  },
+  {
+    "revision": "b4c8141244fa13823940504d33555ecd",
+    "url": "/Rohan_Invitation/static/media/carnival.b4c81412.gif"
+  },
+  {
+    "revision": "b67d985f84e28c9d0b31683072e96e81",
+    "url": "/Rohan_Invitation/static/media/Nikasi.b67d985f.gif"
+  },
+  {
+    "revision": "03c2984ed7b36ed9ce08177b312017b3",
+    "url": "/Rohan_Invitation/static/media/Phera.03c2984e.gif"
+  },
+  {
+    "revision": "d9eee02b07d291eb097c413c1d28b04d",
+    "url": "/Rohan_Invitation/static/media/awesome.d9eee02b.gif"
+  },
+  {
+    "revision": "7b84626804ebe3a7d86bfb7afa18a234",
+    "url": "/Rohan_Invitation/static/media/second.7b846268.jpeg"
+  },
+  {
+    "revision": "7409d7691ddca34f75b155583093d90e",
+    "url": "/Rohan_Invitation/static/media/selfies.7409d769.gif"
+  },
+  {
+    "revision": "28e6635ead6c8d09dd98fb1e56e708d9",
+    "url": "/Rohan_Invitation/static/media/tick.28e6635e.mp3"
+  },
+  {
+    "revision": "b05b0ccb8a72ab9bae493670ed1e895e",
+    "url": "/Rohan_Invitation/index.html"
+  }
+];
